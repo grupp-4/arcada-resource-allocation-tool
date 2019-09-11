09a4e64879007fb492f153da8c3719fe4118db80
Sebastian <lundgres@arcada.fi>
2019-09-12 00:11:30 +0300

    Ändringar för att få navbaren till headen.

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,9 +1,11 @@
 import Grid from '@material-ui/core/Grid';
+import styles from "./styles.scss"
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 export default function GroupedButtons() {
   return (
+    <div className={`${styles.gridStyles} ${styles.gridNavbar}`}>
     <Grid container spacing={3}>
       <Grid item xs={12} md={6}>
         <Grid container spacing={1} direction="column" alignItems="center">
@@ -18,5 +20,6 @@ export default function GroupedButtons() {
         </Grid>
       </Grid>
     </Grid>
+    </div>
   );
 }
