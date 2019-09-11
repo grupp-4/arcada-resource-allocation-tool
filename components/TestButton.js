b7db66de3b26575f5dbd374b9489603b67e3ef2b
Sebastian <lundgres@arcada.fi>
2019-09-11 23:56:10 +0300

    Skapade rätt knappar för navbaren

--- components/TestButton.js
+++ components/TestButton.js
@@ -1,47 +1,22 @@
-import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    margin: theme.spacing(1),
-  },
-  input: {
-    display: 'none',
-  },
-}));
-
-export default function ContainedButtons() {
-  const classes = useStyles();
-
+export default function GroupedButtons() {
   return (
-    <div>
-      <Button variant="contained" className={classes.button}>
-        Default
-      </Button>
-      <Button variant="contained" color="primary" className={classes.button}>
-        Primary
-      </Button>
-      <Button variant="contained" color="secondary" className={classes.button}>
-        Secondary
-      </Button>
-      <Button variant="contained" color="secondary" disabled className={classes.button}>
-        Disabled
-      </Button>
-      <Button variant="contained" href="#contained-buttons" className={classes.button}>
-        Link
-      </Button>
-      <input
-        accept="image/*"
-        className={classes.input}
-        id="contained-button-file"
-        multiple
-        type="file"
-      />
-      <label htmlFor="contained-button-file">
-        <Button variant="contained" component="span" className={classes.button}>
-          Upload
-        </Button>
-      </label>
-    </div>
+    <Grid container spacing={3}>
+      <Grid item xs={12} md={6}>
+        <Grid container spacing={1} direction="column" alignItems="center">
+          <Grid item>
+            <ButtonGroup color="primary" aria-label="outlined primary button group">
+              <Button>Hem</Button>
+              <Button>Sök</Button>
+              <Button>Överblick</Button>
+              <Button>Placeholder</Button>
+            </ButtonGroup>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
