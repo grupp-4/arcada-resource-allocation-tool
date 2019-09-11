67bb6738cb8f4ba4a667fdc584b67cecd01a74e2
Johan Penttinen <johanlepenttinen@gmail.com>
2019-09-11 20:21:49 +0300

    Adds tab component to ContentComponent

--- components/content.js
+++ components/content.js
@@ -4,11 +4,13 @@ import DenseTable from "components/material-table.js";
 import SubmitButton from "components/SubmitButton.js";
 import DiscardButton from "components/DiscardButton.js";
 import MyImage from "components/Image.js";
+import SimpleTabs from "components/tab-switcher-buttons.js"
 
 
 function ContentComponent() {
     return (
         <div className={`${styles.gridStyles} ${styles.gridMain}`}>
+            <SimpleTabs />
             <DenseTable />
             <DenseTable />
             <SubmitButton /><DiscardButton />
