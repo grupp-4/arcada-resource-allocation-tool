db7a5c3c5fdb33634eaf06dc0076d83c7b24041b
Sebastian <lundgres@arcada.fi>
2019-09-11 23:59:28 +0300

    Renamed Testbutton to Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import ExampleComponent from "components/example-component";
 import HandelserComponent from "components/handelser.js";
+import GroupedButtons from "components/NavBar.js"
 import HeaderComponent from "components/header.js";
 import ContentComponent from "components/content.js";
 import styles from "../components/styles.scss";
 import MyImage from "components/Image.js";
+
 const uuidv4 = require('uuid');
 
 import Header from '../components/nextTutorial.js';
@@ -12,7 +14,7 @@ function Index() {
     return (
         <>
             <div className={styles.gridContainer}>
-                <HeaderComponent /> <HandelserComponent /> <ContentComponent />
+                <HeaderComponent /> <GroupedButtons /> <HandelserComponent /> <ContentComponent /> 
             </div>
         </>
     );
