55154e3ad1e9c8252a2c547c10bed8055b214606
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2019-09-11 21:55:57 +0300

    Adds example of using React Hooks
    - 2 new dependencies: classnames and gillog
    - adds `env` entry in next.config.js to make select environment variables available clientside
    - adds _document.js for adding links to the HTML head
    - Webstorm users: edited inspection level for unused exports for files in the pages directory

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const customPathAliases = require("./resolve.alias")
 module.exports = withSass({
     cssModules: true, // Enabling CSS Modules
     sassLoaderOptions: sassLoaderOptions, // Setting the SASS loader options defined in the `sass-loader.config.js` file
+    env: {
+        LOGLEVEL: process.env.LOGLEVEL, // Making the `LOGLEVEL` environment variable available client-side
+    },
     webpack(config) {
         config.resolve.alias = {...config.resolve.alias, ...customPathAliases} // Adding the custom path aliases from the `resolve.alias.js` file
         return config
