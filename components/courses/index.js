f415e366317a0743d2a91f0cf47a37b5a532b57b
Kristoffer Kuvaja Adolfsson <kuvajaan@arcada.fi>
2019-09-20 13:18:09 +0300

    Starts nested lists for courses

--- components/courses/index.js
+++ components/courses/index.js
@@ -4,20 +4,49 @@ import Typography from "@material-ui/core/Typography"
 
 import useTypographyStyles from "styles/typography"
 
-function Courses({log, data}) {
+import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 
+
+function Courses({log, data}) {
+    
+    const [open, setOpen] = React.useState(true);
+  
+    function handleClick() {
+      setOpen(!open);
+    }
     // ====== HOOKS ======>
-    const typographyStyles = useTypographyStyles()
+    //const typographyStyles = useTypographyStyles()
 
     // ====== RENDER ======>
     // TODO: create this component
     return (
-        <Typography className={typographyStyles.typography} variant={"body1"}>
+      <List component="nav" aria-label="main mailbox folders">
             {data && data.courses
-                ? data.courses.map(course => <>{course.name}<br/></>)
-                : "Loading teachers..."}
-        </Typography>
-    )
+                ? data.courses.map(course => 
+                <><ListItem button onClick={handleClick}>
+                    <ListItemText primary={course.name} />
+                    {open ? <ExpandLess /> : <ExpandMore />}
+                </ListItem>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItem button>
+            <ListItemText primary={course.courseCode} />
+            <ListItemText primary={course.hours} />
+            <ListItemText primary={course.period} />
+            <ListItemText primary={course.program} />
+          </ListItem>
+        </List>
+      </Collapse></>
+      ) : "Loading courses..."}
+                
+        </List>
+    );
 }
 
 Courses.id = "courses"
