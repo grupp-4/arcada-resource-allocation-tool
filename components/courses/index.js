f1ad6da29e607882dd406afed6f42fbe12057aba
Kristoffer Kuvaja <kuvajaan@users.noreply.github.com>
2019-09-25 21:25:53 +0300

    Sets Cards into Grid layout, adds Avatars

--- components/courses/index.js
+++ components/courses/index.js
@@ -2,24 +2,25 @@ import React from 'react';
 import {withLogging} from "gillog"
 
 import Typography from "@material-ui/core/Typography"
-
 import useTypographyStyles from "styles/typography"
 
+import Grid from "@material-ui/core/Grid";
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
-
-/*const useStyles = makeStyles({
+/*Styles
+** NOTES:
+** 1. Had to add card margin and remove grid spacing to prevent horizontal scrollbar
+*/
+const useStyles = makeStyles({
   card: {
     minWidth: 50,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+    margin: 4,
   },
   title: {
     fontSize: 14,
@@ -27,46 +28,58 @@ import { makeStyles } from '@material-ui/core/styles';
   pos: {
     marginBottom: 12,
   },
-});*/
+});
+
+//Prototype that takes the first letter of a string and makes it into a Material UI Avatar
+//Call this function with "String".makeAvatar()
+String.prototype.makeAvatar = function() {
+  return <Avatar>{this.charAt(0)}</Avatar>;
+}
 
 
 function Courses({log, data}) {
     
-  //const classes = useStyles();
+  const classes = useStyles();
   
-    /*function handleClick() {
-      setOpen(!open);
-    }*/
     // ====== HOOKS ======>
     //const typographyStyles = useTypographyStyles()
 
     // ====== RENDER ======>
-    // TODO: Fix classNames
+    // Fixed: Avatar
+    // Fixed: Display cards in a grid
     // TODO: If teacher : else button add teacher
+      // Alternative: Make dropdown with default none : Teachers
+    // Fixed: Fix some kind of bug classNames -- Had missed constant? Not sure what fixed this
+    // TODO Searchbox && function
     return (
-
-      <>
-            {data && data.courses
-                ? data.courses.map(course => 
-                  <Card /*className={classes.card}*/>
-                    <CardContent>
-                      <Typography variant="h5" component="h2">
-                      {course.name}
-                      </Typography>
-                      <Typography /*className={classes.pos}*/ color="textSecondary">
-                      {'Program: '+course.program}
-                      <br/>
-                      {'Code: '+course.courseCode}
-                      <br/>
-                      {'Period: '+course.period+' - '+course.hours+'h'}
-                      </Typography>
-        </CardContent>
-        <CardActions>
-        <Button size="small">Add Teacher</Button>
-      </CardActions>
-      </Card>
+  <>
+    <Grid container spacing={0} justify="center" alignItems="stretch">
+      {data && data.courses
+          ? data.courses.map(course => 
+          <Grid item xs={6}>
+            <Card className={classes.card}>
+              <CardHeader
+                avatar={course.name.makeAvatar()}
+                title={course.name}
+                subheader={'Code: '+course.courseCode}
+                />
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                </Typography>
+                <Typography className={classes.pos} color="textSecondary">
+                {'Program: '+course.program}
+                <br/>
+                {'Time: '+course.hours+'h - Starting period '+course.period}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Add Teacher</Button>
+              </CardActions>
+            </Card>
+          </Grid>
       ) : "Loading courses..."}
-          </>      
+    </Grid>
+  </>      
     );
 }
 
