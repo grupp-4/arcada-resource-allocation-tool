c3794bdaf66137e492cb15a4d91a5db3d5cf26ca
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2019-09-25 23:18:58 +0300

    Adds cosmetic languages menu

--- components/persistent-layout/header/index.js
+++ components/persistent-layout/header/index.js
@@ -10,8 +10,10 @@ import Toolbar from "@material-ui/core/Toolbar"
 import IconButton from "@material-ui/core/IconButton"
 import MenuIcon from "@material-ui/icons/Menu"
 import Typography from "@material-ui/core/Typography"
+import TranslateRoundedIcon from "@material-ui/icons/TranslateRounded"
 import MoreVertIcon from "@material-ui/icons/MoreVert"
 
+import Languages from "./languages"
 import Preferences from "./preferences"
 
 import useStyles from "./styles"
@@ -23,7 +25,7 @@ function Header({log, appName, preferences, setTheme, mobile, pathname, strings}
     // ====== HOOKS ======>
 
     const styles = useStyles()
-    const [anchorElement, setAnchorElement] = useState(null)
+    const [{langAnchor, prefAnchor}, setState] = useState({langAnchor: null, prefAnchor: null})
 
     // ====== EVENT HANDLERS ======>
 
@@ -31,12 +33,20 @@ function Header({log, appName, preferences, setTheme, mobile, pathname, strings}
         // TODO: implement menu
         log.debug("User tried opening menu which isn't yet implemented.")
     }
+    function openLanguagesMenu({currentTarget}) {
+        setState(prevState => ({...prevState, ...{langAnchor: currentTarget}}))
+        log.debug("Opening languages menu.")
+    }
+    function closeLanguagesMenu() {
+        setState(prevState => ({...prevState, ...{langAnchor: null}}))
+        log.debug("Closing languages menu.")
+    }
     function openPreferencesMenu({currentTarget}) {
-        setAnchorElement(currentTarget)
+        setState(prevState => ({...prevState, ...{prefAnchor: currentTarget}}))
         log.debug("Opening preferences.")
     }
     function closePreferencesMenu() {
-        setAnchorElement(null)
+        setState(prevState => ({...prevState, ...{prefAnchor: null}}))
         log.debug("Closing preferences.")
     }
 
@@ -54,6 +64,10 @@ function Header({log, appName, preferences, setTheme, mobile, pathname, strings}
                         <Typography className={styles.appName} variant={"h6"}>
                             {appName}
                         </Typography>
+                        <IconButton onClick={openLanguagesMenu} color={"inherit"} size={mobile ? "medium" : "small"}
+                                    aria-controls={"languages-menu"} aria-haspopup={"true"} aria-label={"languages"}>
+                            <TranslateRoundedIcon/>
+                        </IconButton>
                         <IconButton onClick={openPreferencesMenu} color={"inherit"} size={mobile ? "medium" : "small"}
                                     aria-controls={"preferences-menu"} aria-haspopup={"true"} aria-label={"preferences"}>
                             <MoreVertIcon/>
@@ -61,8 +75,11 @@ function Header({log, appName, preferences, setTheme, mobile, pathname, strings}
                     </Toolbar>
                 </Container>
             </AppBar>
+            <Languages
+                anchorEl={langAnchor}
+                onClose={closeLanguagesMenu}/>
             <Preferences
-                anchorEl={anchorElement}
+                anchorEl={prefAnchor}
                 onClose={closePreferencesMenu}
                 preferences={preferences}
                 setTheme={setTheme}
