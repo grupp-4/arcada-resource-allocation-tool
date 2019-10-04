4539922fadbefe6259a7aca5573cedafdfc66e67
Sebastian <lundgres@arcada.fi>
2019-10-04 11:01:40 +0300

    This adds a nonfunctioning placeholder card in the event feed

--- components/events-feed/index.js
+++ components/events-feed/index.js
@@ -5,10 +5,45 @@ import Typography from "@material-ui/core/Typography"
 import useTypographyStyles from "styles/typography"
 
 
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
 
 
 
+export default function SimpleCard() {
 
+  return (
+    <Card>
+      <CardContent>
+        <Typography color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          be
+
+          nev
+
+          lent
+        </Typography>
+        <Typography color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
+/* Detta är äldre men finare kod gjort av Daniel och Johan
 function EventsFeed({log, cs, strings}) {
 
 
@@ -25,7 +60,8 @@ function EventsFeed({log, cs, strings}) {
         <Typography className={typographyStyles.typography} variant={"body1"}>
             cs.map
         </Typography>
+        
     )
 }
-
-export default withLogging(EventsFeed)
+*/
+// export default withLogging(EventsFeed)
